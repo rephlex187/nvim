c24815e8254c579ffcffd3ed6f19e25f645cf106
Moody <info@moodyprogramming.de>
2024-11-07 11:45:46 +0100

    fix: remove oil h mapping

--- lua/sytec/plugins/oil.lua
+++ lua/sytec/plugins/oil.lua
@@ -6,7 +6,6 @@ return {
       -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
       prompt_save_on_select_new_entry = true,
       keymaps = {
-        ["h"] = "actions.parent",
         ["<C-s>"] = false,
         ["<C-h>"] = false,
         ["<C-t>"] = false,
