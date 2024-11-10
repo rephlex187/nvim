c4f05ebf794ead23bcf774f036414c58d23437cc
Moody <enzoaphexprod@gmail.com>
2024-11-10 10:22:28 +0100

    remap tmux conf

--- lua/sytec/plugins/vim-tmux-navigator.lua
+++ lua/sytec/plugins/vim-tmux-navigator.lua
@@ -1,16 +1,16 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	cmd = {
-		"TmuxNavigateLeft",
-		"TmuxNavigateDown",
-		"TmuxNavigateUp",
-		"TmuxNavigateRight",
-		"TmuxNavigatePrevious",
-	},
-	keys = {
-		{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-		{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-		{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-	},
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+  },
+  keys = {
+    { "<c-0>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-1>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-2>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-3>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+  },
 }
