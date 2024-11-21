0c0a4c3784ac383b555045734dd0fae8d511e16f
Moody Sib <info@moodyprogramming.de>
2024-11-21 18:02:59 +0100

    autocmd: add xclip after yank

--- lua/sytec/autocommands.lua
+++ lua/sytec/autocommands.lua
@@ -1,36 +1,37 @@
 -- Use 'q' to quit from common plugins
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
-  callback = function()
-    vim.cmd([[
+    pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+    callback = function()
+        vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR>
       set nobuflisted
     ]])
-  end,
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "*" },
-  callback = function()
-    vim.cmd([[
+    pattern = { "*" },
+    callback = function()
+        vim.cmd([[
       autocmd BufEnter * set formatoptions-=cro
       autocmd BufEnter * setlocal formatoptions-=cro
     ]])
-  end,
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufRead" }, {
-  pattern = { "neomutt*" },
-  callback = function()
-    require("cmp").setup.buffer({ enabled = false })
-  end,
+    pattern = { "neomutt*" },
+    callback = function()
+        require("cmp").setup.buffer({ enabled = false })
+    end,
 })
 
 -- Highlight Yanked Text
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank({ higroup = "Visual", timeout = 300 })
-  end,
+    callback = function()
+        vim.fn.system("xclip -sel clip", vim.fn.getreg("\""))
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    end,
 })
 
 vim.api.nvim_create_user_command("W", "noautocmd w", { bang = true })
@@ -38,7 +39,7 @@ vim.api.nvim_create_user_command("Wq", "noautocmd wq", { bang = true })
 
 local blade_group = vim.api.nvim_create_augroup("BladeFiletypeRelated", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  group = blade_group,
-  pattern = "*.blade.php",
-  command = "set ft=blade",
+    group = blade_group,
+    pattern = "*.blade.php",
+    command = "set ft=blade",
 })
